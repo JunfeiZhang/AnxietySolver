af4731440218608d2b1eb4b460ece707b0ab5e0e
Junfei Zhang <zhangjunfly@gmail.com>
2017-01-15 09:28:25 -0800

    demo

--- app.js
+++ app.js
@@ -95,18 +95,48 @@ bot.dialog('/', [
             ]);
             session.send(msg);
         builder.Prompts.choice(session, "Do you like it?", "Yes,show me more|No, anything else?"); 
+        
     },
-    function (session, result) {
-        if (results.response) {
-            if(results.response.entity == 1){
-                session.beginDialog('/img');
-            } else if(results.response.entity == 2){
-                session.beginDialog('/joke');
-            }
-        } else {
-            session.send("ok");
-        }
-    }
+    function (session, next) {
+        // builder.Prompts.text(session, "img");
+        var joke = jokes[0];
+        session.send("%(content)s", joke); 
+        builder.Prompts.choice(session, "Do you like it?", "Yes,show me more|No, anything else?"); 
+    },function(session, next){
+        msg = new builder.Message(session)
+            .textFormat(builder.TextFormat.xml)
+            .attachments([
+                new builder.VideoCard(session)
+                    .title("Anxiety Relief Music")
+                    .subtitle("YouTube")
+                    .text("This is a anxiety relief music that helps you relax and refresh yourself. Help you enjoy!")
+                    .media([
+                        builder.CardMedia.create(session, "https://r6---sn-p5qlsnll.googlevideo.com/videoplayback?sparams=dur%2Cei%2Cid%2Cinitcwndbps%2Cip%2Cipbits%2Citag%2Clmt%2Cmime%2Cmm%2Cmn%2Cms%2Cmv%2Cnh%2Cpl%2Cratebypass%2Crequiressl%2Csource%2Cupn%2Cexpire&expire=1484518291&ipbits=0&lmt=1471441162439184&initcwndbps=1161250&source=youtube&ms=au&mv=m&mt=1484496653&dur=7120.817&requiressl=yes&id=o-ADb1T11pcWZl1hKSiOx7XOFFngrbEfFW9xq8wCBLg29Q&pl=24&mn=sn-p5qlsnll&mm=31&upn=ipC97u5-lDg&ratebypass=yes&ei=Mp97WJXHO4rV8gS3xa2QCw&signature=79812792DE2A6F80701D45853A4C7D253ACE116A.DF83CBFBC5D98BA8CEEC5967801773405A24AA56&key=yt6&itag=22&mime=video%2Fmp4&nh=IgpwcjAzLmlhZDA3Kg0xOTguMjcuNzMuMTA0&ip=192.99.209.145&signature=&title=2%20HOURS%20OF%20CAT%20MUSIC%20-%20RELAXING%20MUSIC%20FOR%20CATS%20-%202%20HORA%20GATO%20M%C3%9ASICA%20-%20RELAXING%20SOUNDS&filename=2%20HOURS%20OF%20CAT%20MUSIC%20-%20RELAXING%20MUSIC%20FOR%20CATS%20-%202%20HORA%20GATO%20M%C3%9ASICA%20-%20RELAXING%20SOUNDS.mp4")
+                    ])
+                    .autoloop(true)
+                    .autostart(false)
+                    .shareable(true)                    
+            ]);
+        session.send(msg);
+        builder.Prompts.choice(session, "Do you like it?", "Yes,show me more|No, anything else?"); 
+    },
+    function (session, next) {
+         var msg = new builder.Message(session)
+            .textFormat(builder.TextFormat.xml)
+            .attachments([
+                new builder.ThumbnailCard(session)
+                    .title("Relaxing Game")
+                    .subtitle("Go ahead and have fun!")
+                    .images([
+                        builder.CardImage.create(session, "http://d13yacurqjgara.cloudfront.net/users/44967/screenshots/365048/i-will-never-lego.jpg")
+                    ])
+                    .buttons([
+                        builder.CardAction.openUrl(session, "http://www.shaunthesheep.com/games", "Play Game"),
+                    ]),
+            ]);
+        session.endDialog(msg);
+        session.Prompts.text(session, "byebye");
+}
     ]);
 
 bot.dialog('/img', [
