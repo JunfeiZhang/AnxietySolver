41c1f75304195b4327827e629501c0e91aa04827
Junfei Zhang <zhangjunfly@gmail.com>
2017-01-14 18:48:21 -0800

    base version

--- app.js
+++ app.js
@@ -17,12 +17,33 @@ var connector = new builder.ChatConnector({
     appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
 var bot = new builder.UniversalBot(connector);
+var intents = new builder.IntentDialog();
 server.post('/api/messages', connector.listen());
 
 //=========================================================
 // Bots Dialogs
 //=========================================================
 
-bot.dialog('/', function (session) {
-    session.send("Hello World");
-});
+bot.dialog('/', [
+    function (session, args, next) {
+        if (session.userData.name) {
+            session.beginDialog('/profil');
+        } else {
+            next();
+        }
+    },
+    function (session, results) {
+        session.send('Hello %s!', session.userData.name);
+    }
+]);
+
+bot.dialog('/profil', [
+    function (session) {
+        builder.Prompts.text(session, 'Hi! What is your name?');
+    },
+    function (session, results) {
+        console.log("test: "+results.response);
+        session.userData.name = results.response;
+        session.endDialog();
+    }
+]);
